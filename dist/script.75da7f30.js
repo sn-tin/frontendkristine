c2d2465004664facd82db85f08fc1dd4bcd1f108
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-06-11 18:06:33 +0800

    Add active-page class on each page

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -121,41 +121,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 $('.buttons .choose-projects').click(function (event) {
   console.log(event.target);
   var targetButton = $(event.target);
+  $(".buttons .choose-projects").removeClass("active-button");
 
   if (targetButton.hasClass("personalproj-btn")) {
     $(this).addClass("active-button");
     $(".personal-projects").removeClass("d-none");
-    $(".fem-btn").removeClass("active-button d-block");
     $(".frontendmentor").addClass("d-none");
   }
 
   if (targetButton.hasClass("fem-btn")) {
     $(this).addClass("active-button");
     $(".frontendmentor").removeClass("d-none");
-    $(".personalproj-btn").removeClass("active-button");
     $(".personal-projects").addClass("d-none");
-  }
-});
-$('list-of-pages .pages').click(function (event) {
-  console.log(event.target);
-  var targetLink = $(event.target);
-
-  if (targetLink.hasClass("project-page")) {
-    $(this).addClass("active-page");
-    $(".about-me-page").removeClass("active-page");
-    $(".contact-page").removeClass("active-page");
-  }
-
-  if (targetLink.hasClass("about-me-page")) {
-    $(this).addClass("active-page");
-    $(".project-page").removeClass("active-page");
-    $(".contact-page").removeClass("active-page");
-  }
-
-  if (targetLink.hasClass("contact-page")) {
-    $(this).addClass("active-page");
-    $(".project-page").removeClass("active-page");
-    $(".about-me-page").removeClass("active-page");
   }
 });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
@@ -186,7 +163,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44153" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36177" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/script.js
+++ src/script.js
@@ -1,36 +1,15 @@
 $('.buttons .choose-projects').click(function(event){
     console.log(event.target)
     let targetButton = $(event.target)
+    $(".buttons .choose-projects").removeClass("active-button")
     if(targetButton.hasClass("personalproj-btn")){
         $(this).addClass("active-button")
         $(".personal-projects").removeClass("d-none")
-        $(".fem-btn").removeClass("active-button d-block")
         $(".frontendmentor").addClass("d-none")
     }
     if(targetButton.hasClass("fem-btn")){
         $(this).addClass("active-button")
         $(".frontendmentor").removeClass("d-none")
-        $(".personalproj-btn").removeClass("active-button")
         $(".personal-projects").addClass("d-none")
-    }
-})
-
-$('list-of-pages .pages').click(function(event){
-    console.log(event.target)
-    let targetLink = $(event.target)
-    if(targetLink.hasClass("project-page")){
-        $(this).addClass("active-page")
-        $(".about-me-page").removeClass("active-page")
-        $(".contact-page").removeClass("active-page")
-    }
-    if(targetLink.hasClass("about-me-page")){
-        $(this).addClass("active-page")
-        $(".project-page").removeClass("active-page")
-        $(".contact-page").removeClass("active-page")
-    }
-    if(targetLink.hasClass("contact-page")){
-        $(this).addClass("active-page")
-        $(".project-page").removeClass("active-page")
-        $(".about-me-page").removeClass("active-page")
     }
 })
