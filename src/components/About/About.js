09d8592804e171eaf5af4a9b924f8d8f35f04d2e
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-11-07 21:55:09 +0800

    Add about content

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,30 @@
+import { StyleAbout } from '../About/About.styles'
 
 const About = () => {
     return (
-        <h1>About</h1>
+        <StyleAbout>
+            <h1>About</h1>
+            <div className='about-content'>
+                <div className='personal'>
+                    <h2>Kristine May de Jesus</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                        qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div className='tools'>
+                    <h2>Tools I use</h2>
+                    <p>
+                    HTML, CSS, Bootstrap,
+                    SCSS/SASS, JavaScript,
+                    ReactJS, jQuery, Webpack,
+                    Parcel, Git, Ubuntu
+                    </p>
+                </div>
+            </div>
+        </StyleAbout>
     )
 }
 
---
+++ src/components/About/About.styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyleAbout = styled.div`
+    width: 100%;
+    max-width: 1246px;
+    margin: 0 auto;
+`
+
+export { StyleAbout }
