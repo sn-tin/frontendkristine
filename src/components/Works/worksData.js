84580041852880d7768a9a0fa02567fb0634797d
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-11-13 12:20:47 +0800

    Change id

--- src/components/Works/worksData.js
+++ src/components/Works/worksData.js
@@ -5,28 +5,28 @@ import rps from '../../assets/rps.png';
 
 let works = [
     {
-        id: 0,
+        id: 1,
         img: noContext,
         name: "No Context",
         date: "October 2022",
         category: "Personal Project"
     },
     {
-        id: 1,
+        id: 2,
         img: shortly,
         name: "Shortly",
         date: "September 2022",
         category: "Solution"
     },
     {
-        id: 2,
+        id: 3,
         img: todo,
         name: "To-Do List",
         date: "August 2022",
         category: "Personal Project"
     },
     {
-        id: 3,
+        id: 4,
         img: rps,
         name: "Rock Paper Scissors",
         date: "July 2022",
