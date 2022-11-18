ec88afdccc5a8a3c4c97a0d407b5dae92d3a9b66
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-11-18 19:48:59 +0800

    Changes in home section - Remove unneccessary comments - Change text alignment to center

--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -2,45 +2,37 @@ import styled from "styled-components";
 import { devices, theme } from "../../styles";
 
 const Content = styled.div`
-    /* width: 100%;
-    max-width: 1216px; */
     margin: 0 auto;
     display: flex;
     flex-direction: column;
     justify-content: center;
+    text-align: center;
     h1 {
         font-size: ${theme.heading.bigger};
     }
     p {
         width: 100%;
-        max-width: 909px;
-        font-size: clamp(1.438rem, 5vw, 2.813rem);
+        max-width: 800px;
+        font-size: clamp(1.438rem, 5vw, 2rem);
         font-weight: ${theme.fontWeights.bold};
-        text-align: right;
-        margin: 40px 0 40px auto;
+        margin: 0 auto;
     }
     @media screen and (${devices.laptop}) {
         p {
-            margin: 53px 0 53px auto;
+            margin: 0 auto;
         }
     }
 `
 
 const HomeButton = styled.button`
     font-family: ${theme.heading.fontFam};
-    font-size: clamp(0.938rem, 2.5vw, 1.563rem);
+    font-size: clamp(0.938rem, 2.5vw, 1.3rem);
     display: flex;
     align-items: center;
     background-color: transparent;
     padding: 8px 15px;
-    margin-left: auto;
+    margin: 30px auto;
     cursor: pointer;
-    /* &:hover > svg {
-        position: relative;
-        left: 0.8rem;
-        transform: scale(1.2);
-        transition: all 0.2s ease-in-out;
-    } */
     @media screen and (${devices.laptop}) {
         padding: 15px 30px;
     }
