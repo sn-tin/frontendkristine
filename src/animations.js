edc5b7788532c4a03e882a8a047f50174e080d31
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2022-11-25 16:08:15 +0800

    Remove unneccessary code in animation

--- src/animations.js
+++ src/animations.js
@@ -1,11 +1,11 @@
 /* NAVBAR AND FOOTER LINES ANIMATION */
 const linesAnimate = {
     start: {
-        transform: "translate3d(0px, 0px, 0px) scale3d(0, 0, 0) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+        transform: "scale3d(0, 0, 0)",
         transformStyle: "preserve-3d",
     },
     end: {
-        transform: "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+        transform: "scale3d(1, 1, 1)",
         transformStyle: "preserve-3d",
         transition: {
             duration: 1.5,
