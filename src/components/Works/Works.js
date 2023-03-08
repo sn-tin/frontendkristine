e5ef17a497b2c6e6eab295fb527d2280d6e518c6
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-03-08 21:14:09 +0800

    change lg value of grid

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -4,13 +4,20 @@ import works from "./worksData";
 import { Grid } from "@mui/material";
 
 const Works = () => {
+    const lgValue = (id) => {
+        if (id === works.length && works.length % 2 !== 0) {
+            return 12
+        } else {
+            return 6
+        }
+    }
     return (
         <>
         <StyledWork id="works">
             <WorkContainer className="works">
                 <Grid container spacing={5}>
                     { works.map((work) => 
-                        <Grid item xs="12" lg={work.id === works.length ? "12" : "6"}>
+                        <Grid item xs="12" lg={lgValue(work.id)}>
                             <Cards key={work.id} id={work.id} works={work} />
                         </Grid>).reverse()
                     }
