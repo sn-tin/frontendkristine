bf84a62a4663b3464842623f7d38bd3c0e991457
Kristine May A. de Jesus <kristinemayadejesus@gmail.com>
2023-03-08 18:45:53 +0800

    remove comment

--- src/components/Works/Works.js
+++ src/components/Works/Works.js
@@ -10,10 +10,6 @@ const Works = () => {
         <>
         <StyledWork id="works">
             <WorkContainer className="works">
-                {/* <WorkTexts className="title-description">
-                    <motion.h2 variants={rightAnimate} initial="start" animate="end" exit="exit">Explore my works</motion.h2>
-                    <motion.p variants={rightAnimate} initial="start" animate="end" exit="exit">Check out the documentations for some of the learning projects I’ve made where I was able to to use the skills I developed learning front-end tech stacks.</motion.p>
-                </WorkTexts> */}
                 <Grid container spacing={5}>
                     { works.map((work) => 
                         <Grid item xs="12" lg={work.id === works.length ? "12" : "6"}>
